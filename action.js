673164ea21df7bc0a3518d98b80bc7b3fa6ef7f9
dheeraj <dheeraj.br2@gmail.com>
2023-08-16 20:38:34 +0530

    s3

--- action.js
+++ action.js
@@ -1,18 +1,9 @@
-function counterFn() {
-  let count = 0;
+// currying
 
-  return {
-    increment10: () => (count = count + 10),
-    increment1: () => (count = count + 1),
-    getValue: () => count,
+function multiply(a) {
+  return (b) => {
+    return a * b;
   };
 }
 
-const counter = counterFn();
-
-counter.increment10();
-counter.increment1();
-counter.increment1();
-counter.increment1();
-counter.increment1();
-console.log(counter.getValue());
+console.log(multiply(2642)(2));
