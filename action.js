b6db8caf0e547d25b27430d3def4a48cf09031d8
dheeraj <dheeraj.br2@gmail.com>
2023-08-16 20:58:53 +0530

    s5

--- action.js
+++ action.js
@@ -1,8 +1,8 @@
-// pure function and array mutate
+// concatenate arrays
 
-let names = ['name1', 'name2', 'name3', 'name4'];
-
-function appendToArr(names, elem) {
-  return [...names, elem]; // do not mutate arrays and obj, use spread
+function concatArr(arr1, arr2, arr3) {
+  // return arr1.concat(arr2, arr3);
+  return [...arr1, ...arr2, ...arr3];
 }
-console.log(names, appendToArr(names, 'name5'));
+
+console.log(concatArr([132, 123, 1, 123], [13, 8756, 2, 521, 212], [234354]));
