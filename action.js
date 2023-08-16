60984dd032f19533095bbc23f5c7ee5a57f6fabc
dheeraj <dheeraj.br2@gmail.com>
2023-08-16 20:26:26 +0530

    s2

--- action.js
+++ action.js
@@ -1,26 +1,18 @@
-const user = [
-  { id: 234, user: 'name1', isActive: true, age: 200 },
-  { id: 246, user: 'name2', isActive: false, age: 0 },
-  { id: 34, user: 'name3', isActive: true, age: 50 },
-];
+function counterFn() {
+  let count = 0;
 
-// let names = [];
-// user.forEach((obj) => {
-//   names.push(obj.name);
-// });
+  return {
+    increment10: () => (count = count + 10),
+    increment1: () => (count = count + 1),
+    getValue: () => count,
+  };
+}
 
-// all names
-let userNames = user.map((obj) => obj.user);
+const counter = counterFn();
 
-// active user names
-let activeUsers = user.filter((obj) => obj.isActive).map((obj) => obj.user);
-
-// active user names sorted by age
-let activeUsersAsc = user
-  .filter((obj) => obj.isActive)
-  .sort((obj, obj1) => obj.age - obj1.age)
-  .map((obj) => obj.user);
-
-console.log(activeUsers, userNames, activeUsersAsc);
-
-console.log(typeof null); // object
+counter.increment10();
+counter.increment1();
+counter.increment1();
+counter.increment1();
+counter.increment1();
+console.log(counter.getValue());
