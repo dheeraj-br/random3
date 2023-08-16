468d196dc3aeb3b6af7f5c5d6420827898787dc3
dheeraj <dheeraj.br2@gmail.com>
2023-08-16 21:36:54 +0530

    s6

--- action.js
+++ action.js
@@ -1,8 +1,23 @@
-// concatenate arrays
+// check if array obj exists by key value
 
-function concatArr(arr1, arr2, arr3) {
-  // return arr1.concat(arr2, arr3);
-  return [...arr1, ...arr2, ...arr3];
-}
+let user = [
+  { id: 234, user: 'name1', isActive: true, age: 200 },
+  { id: 246, user: 'name2', isActive: false, age: 0 },
+  { id: 34, user: 'name3', isActive: true, age: 50 },
+];
 
-console.log(concatArr([132, 123, 1, 123], [13, 8756, 2, 521, 212], [234354]));
+// console.log(!!user.find((obj) => obj.user === 'name1'));
+
+// remove duplicates
+let arr = [1, 2, 3, 4, 5, 5, 5, 5, 1, 1, 1];
+
+let uniqueElem = [...new Set(arr)];
+// console.log(uniqueElem);
+
+uniqueElem = arr.reduce((aggregator, elem) => {
+  console.log(aggregator, elem);
+  return aggregator.includes(elem) ? aggregator : [...aggregator, elem]; // better than push because push returns index
+  //return value becomes value for aggregator for next iter
+}, []); // initializer for aggregator, if absent takes first elem of array
+
+console.log(uniqueElem);
